c1a14eba1ef25b1d199d0db7b2dab8c870d79d04
PortalArm <evgenii_kim2010@mail.ru>
2020-08-27 20:26:37 +0600

    Add project files.

---
+++ Gomoku Custom.Shared/GameData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Gomoku_Custom.Shared
+{
+    [Serializable]
+    public class GameData
+    {
+        public const int FieldDim = 19;
+        public byte[,] Field { get; set; }
+        public Point Updated { get; set; }
+        //public string Message { get; set; }
+        public ResponseCode Code { get; set; }
+
+        //public static GameData FromByteArray(byte[] input)
+        //{
+        //    return 
+        //}
+
+        //public static byte[] ToByteArray(GameData data)
+        //{
+        //    //byte[] utfString = Encoding.UTF8.GetBytes(data.Message);
+        //    byte[] transfer = new byte[
+        //        data.Field.Length + //Field
+        //        2 + //Point
+        //        //utfString.Length + //Message
+        //        1 //Code
+        //        ];
+        //    int index = 0;
+        //    foreach (var element in data.Field)
+        //        transfer[index++] = element;
+
+        //    transfer[index++] = data.Updated.X;
+        //    transfer[index++] = data.Updated.Y;
+        //    transfer[index] = (byte)data.Code;
+            
+        //    return transfer;
+        //}
+    }
+
+    public enum ResponseCode : byte { OK, PointOccupied, PointOutOfRange, RequestError, Win, Loss, AccessDenied, Reserved }
+    public enum Team { Blue, Red, Unknown }
+    [Serializable]
+    public struct MoveData
+    {
+        public Point Proposed { get; set; }
+        public Guid ClientId { get; set; }
+    }
+    [Serializable]
+    public struct InitData
+    {
+        public Guid ClientId { get; set; }
+        public Team Side { get; set; }
+        public ResponseCode Code { get; set; }
+    }
+    [Serializable]
+    public struct Point
+    {
+        public byte X { get; set; }
+        public byte Y { get; set; }
+
+        public Point(byte x, byte y) =>
+            (X, Y) = (x, y);
+
+    }
+}
---
+++ Gomoku Custom.Shared/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gomoku_Custom.Shared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
