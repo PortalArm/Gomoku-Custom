114f607e2f4a2500ac24493ac735177c34dfbdc3
PortalArm <evgenii_kim2010@mail.ru>
2020-08-29 01:28:17 +0600

    Refactor

---
+++ Gomoku Custom.Client/GomokuClient.cs
@@ -0,0 +1,70 @@
+﻿using Gomoku_Custom.Shared;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gomoku_Custom.Client
+{
+    public class GomokuClient : IDisposable
+    {
+        private readonly HttpClient _client;
+        private readonly BinaryFormatter _bf; // не уверен
+        //private readonly HttpClient _listener;
+        private InitData _init;
+        public GomokuClient(string baseAddress = "http://localhost:5010/game/")
+        {
+            _client = new HttpClient() { BaseAddress = new Uri(baseAddress), Timeout = Timeout.InfiniteTimeSpan };
+            _bf = new BinaryFormatter();
+        }
+
+        public void Dispose() =>
+            ((IDisposable)_client).Dispose();
+
+        //public async Task Start()
+        //{
+        //    var request = new HttpRequestMessage() { Method = HttpMethod.Get };
+        //    using (MemoryStream ms = new MemoryStream())
+        //    {
+        //        object obj = new Point(3, 7);
+        //        _bf.Serialize(ms, obj);
+        //        request.Content = new ByteArrayContent(ms.ToArray());
+        //    }
+        //    var response = await _client.SendAsync(request);
+        //    string str = await response.Content.ReadAsStringAsync();
+        //    Console.WriteLine(str);
+        //}
+        public async Task EstablishConnection()
+        {
+            var request = new HttpRequestMessage() { Method = HttpMethod.Get };
+            var response = await _client.SendAsync(request);
+            Stream stream = await response.Content.ReadAsStreamAsync();
+            var responseObject = _bf.Deserialize(stream);
+
+            _ = responseObject switch { 
+                InitData id => _init = id,
+                _ => throw new InvalidOperationException()
+            };
+            Console.WriteLine("Got my team: {0} and client id: {1}", _init.Side, _init.ClientId);
+        }
+        public async Task Send(object obj)
+        {
+            if (_init.ClientId == Guid.Empty)
+                throw new NotSupportedException();
+
+            var request = new HttpRequestMessage() { Method = HttpMethod.Get };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                _bf.Serialize(ms, obj);
+                request.Content = new ByteArrayContent(ms.ToArray());
+            }
+            var response = await _client.SendAsync(request);
+            string str = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(str);
+        }
+    }
+}
--- Gomoku Custom.Client/Program.cs
+++ Gomoku Custom.Client/Program.cs
@@ -1,8 +1,5 @@
 ﻿using Gomoku_Custom.Shared;
 using System;
-using System.IO;
-using System.Net.Http;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
 
 namespace Gomoku_Custom.Client
@@ -11,7 +8,8 @@ namespace Gomoku_Custom.Client
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Enter to send");
+            Console.WriteLine("Enter to establish connection");
+            Console.ReadLine();
             using (GomokuClient gc = new GomokuClient())
             {
                 await gc.EstablishConnection();
@@ -23,64 +21,6 @@ namespace Gomoku_Custom.Client
                 await gc.Send(new MoveData() { Proposed = new Point(7, 8) });
             }
                 Console.ReadLine();
-        }
-    }
-
-    public class GomokuClient : IDisposable
-    {
-        private readonly HttpClient _client;
-        private readonly BinaryFormatter _bf; // не уверен
-        //private readonly HttpClient _listener;
-        private InitData _init;
-        public GomokuClient(string baseAddress = "http://localhost:5010/game/")
-        {
-            _client = new HttpClient() { BaseAddress = new Uri(baseAddress) };
-            _bf = new BinaryFormatter();
-        }
-
-        public void Dispose() =>
-            ((IDisposable)_client).Dispose();
-
-        //public async Task Start()
-        //{
-        //    var request = new HttpRequestMessage() { Method = HttpMethod.Get };
-        //    using (MemoryStream ms = new MemoryStream())
-        //    {
-        //        object obj = new Point(3, 7);
-        //        _bf.Serialize(ms, obj);
-        //        request.Content = new ByteArrayContent(ms.ToArray());
-        //    }
-        //    var response = await _client.SendAsync(request);
-        //    string str = await response.Content.ReadAsStringAsync();
-        //    Console.WriteLine(str);
-        //}
-        public async Task EstablishConnection()
-        {
-            var request = new HttpRequestMessage() { Method = HttpMethod.Get };
-            var response = await _client.SendAsync(request);
-            Stream stream = await response.Content.ReadAsStreamAsync();
-            var responseObject = _bf.Deserialize(stream);
-
-            _ = responseObject switch { 
-                InitData id => _init = id,
-                _ => throw new InvalidOperationException()
-            };
-            Console.WriteLine("Got my team: {0} and client id: {1}", _init.Side, _init.ClientId);
-        }
-        public async Task Send(object obj)
-        {
-            if (_init.ClientId == Guid.Empty)
-                throw new NotSupportedException();
-
-            var request = new HttpRequestMessage() { Method = HttpMethod.Get };
-            using (MemoryStream ms = new MemoryStream())
-            {
-                _bf.Serialize(ms, obj);
-                request.Content = new ByteArrayContent(ms.ToArray());
-            }
-            var response = await _client.SendAsync(request);
-            string str = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(str);
         }
     }
 }
