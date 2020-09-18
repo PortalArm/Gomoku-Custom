b7d4aa4259f8b90c3f480688adc8edc6640f085b
PortalArm <evgenii_kim2010@mail.ru>
2020-09-18 23:25:15 +0600

    Reinstall js libraries

--- BlazorASP/Startup.cs
+++ BlazorASP/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorASP.Data;
 using Blazored.SessionStorage;
 using System.Text.Json;
 using Gomoku_Custom.Blazor.Data;
@@ -32,8 +31,7 @@ namespace Gomoku_Custom.Blazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddBlazoredSessionStorage();
-            services.AddSingleton<WeatherForecastService>();
-            services.AddSingleton<GameService>(GameService.Instance);
+            services.AddSingleton(GameService.Instance);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- BlazorASP/wwwroot/js/script.js
+++ BlazorASP/wwwroot/js/script.js
@@ -1,9 +1,13 @@
 ﻿var dotnetHelper;
-var disposed = false;
-var initializeField = (size, dotnet) => {
+//var disposed = false;
+var index = 1;
+const teams = ["", "team-blue", "team-red"];
+var isMyTurn = async () => {
+    let result = await dotnetHelper.invokeMethodAsync('IsClientTurn');
+    return result;
+};
+var initializeField = (size, dotnet, extfield) => {
     dotnetHelper = dotnet;
-    disposed = false;
-    console.log(`begin ${size}`);
     const field = document.getElementById("field");
     while(field.lastChild)
        field.lastChild.remove();
@@ -13,7 +17,7 @@ var initializeField = (size, dotnet) => {
         fieldrow.setAttribute("class", "game-field-row");
         for(let j = 0;j < size; ++j){
             var fieldcell = document.createElement("div");
-            fieldcell.setAttribute("class", `game-field-cell`);
+            fieldcell.setAttribute("class", "game-field-cell");
             fieldcell.setAttribute("data-row", i);
             fieldcell.setAttribute("data-col", j);
             fieldcell.addEventListener("click", processClick, { once: true });
@@ -21,35 +25,32 @@ var initializeField = (size, dotnet) => {
         }
         field.appendChild(fieldrow);
     }
-    console.log(`end ${size}`);
+    if(extfield)
+        populateField(extfield);
+    
 };
-var processClick = event => {
+var populateField = field => {
+    for (let i = 0; i < field.length; ++i)
+        for (let j = 0; j < field[i].length; ++j)
+            if(field[i][j] != 0)
+               updateCell(i,j,field[i][j]);
+};
+var processClick = async event => {
+    if (!await isMyTurn()) {
+        console.log("It's not your turn!");
+        event.target.addEventListener("click", processClick, { once: true });
+        return;
+    }
     const target = event.target;
     let row = parseInt(target.getAttribute("data-row"));
     let col = parseInt(target.getAttribute("data-col"));
-    dotnetHelper.invokeMethodAsync('ReceivePos', row, col, teams[index]).then(r => console.log(r));
-    //DotNet.invokeMethodAsync('Gomoku Custom.Blazor', 'SayHello', 'bruh').then(r => console.log(r));
-    if(updateCell(row, col, teams[index])) index = (index + 1) % 2;
-    //console.log(row + " " + col);
+    await dotnetHelper.invokeMethodAsync('ReceivePos', row, col);
 };
-const teams = ["team-red", "team-blue"];
-var index = 0;
 var updateCell = (row, col, color) => {
     const obj = document.querySelector(`.game-field-cell[data-row='${row}'][data-col='${col}']`);
-    if(!obj || obj.getAttribute("class") != 'game-field-cell')
+    if (!obj || obj.getAttribute("class") != 'game-field-cell' || color === "")
         return false;
-    obj.classList.add(color);
+    obj.removeEventListener("click", processClick);
+    obj.classList.add(teams[color]);
     return true;
 };
-document.addEventListener("beforeunload", e => {
-    dotnetHelper.invokeMethodAsync('Dispose');
-    disposed = true;
-});
-//document.onclose = e => {
-//    alert("yep");
-//    if (dotnetHelper !== null)
-//        dotnetHelper.dispose();
-//};
-
-
-//document.addEventListener("DOMContentLoaded",e => initializeField(19));
