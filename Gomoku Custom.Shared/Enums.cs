a8a47436f221f79e99610121d380a5caab1e5e9e
PortalArm <evgenii_kim2010@mail.ru>
2020-09-18 23:25:26 +0600

    Modify shared classes

--- Gomoku Custom.Shared/Enums.cs
+++ Gomoku Custom.Shared/Enums.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace Gomoku_Custom.Shared
 {
-    public enum ResponseCode : byte { OK, PointOccupied, PointOutOfRange, RequestError, Win, Loss, AccessDenied, WrongTurn, Reserved, RuleViolation, ServerClosed,
+    public enum ResponseCode : byte { OK, 
+        PointOccupied, 
+        PointOutOfRange, 
+        RequestError, 
+        Win, 
+        Loss, 
+        AccessDenied, WrongTurn, Reserved, RuleViolation, ServerClosed,
         Draw
     }
     public enum Team { None, Blue, Red, Unknown }
-    public enum GameState { AwaitingConnections, GameStarted, GameEnded }
+    public enum GameState { 
+        AwaitingConnections, 
+        GameStarted, 
+        GameEnded, 
+        WaitingForBlue, 
+        WaitingForRed,
+        AwaitingStart
+    }
 }
--- Gomoku Custom.Shared/GameData.cs
+++ Gomoku Custom.Shared/GameData.cs
@@ -11,37 +11,10 @@ namespace Gomoku_Custom.Shared
     [Serializable]
     public class GameData
     {
-        //public const int FieldDim = 19;
         public Team[][] Field { get; set; }
         public Point Updated { get; set; }
-        //public string Message { get; set; }
         public ResponseCode Code { get; set; }
         public Team NextPlayer { get; set; }
-
-        //public static GameData FromByteArray(byte[] input)
-        //{
-        //    return 
-        //}
-
-        //public static byte[] ToByteArray(GameData data)
-        //{
-        //    //byte[] utfString = Encoding.UTF8.GetBytes(data.Message);
-        //    byte[] transfer = new byte[
-        //        data.Field.Length + //Field
-        //        2 + //Point
-        //        //utfString.Length + //Message
-        //        1 //Code
-        //        ];
-        //    int index = 0;
-        //    foreach (var element in data.Field)
-        //        transfer[index++] = element;
-
-        //    transfer[index++] = data.Updated.X;
-        //    transfer[index++] = data.Updated.Y;
-        //    transfer[index] = (byte)data.Code;
-            
-        //    return transfer;
-        //}
     }
 
     
